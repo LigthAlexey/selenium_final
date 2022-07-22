083bf6c371c26b793f08c05ca4de148a5d137d00
LigthAlexey <karalkin@mail.ru>
2022-07-22 20:55:35 +0700

    1

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose user language")
