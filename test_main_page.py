f984e28d7ee0e8d37081190f8ba637f8cc57cde8
LigthAlexey <karalkin@mail.ru>
2022-07-28 19:42:18 +0700

    add NoSuchElementException

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,16 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
