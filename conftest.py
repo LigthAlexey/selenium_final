af7b74e659d628d6345af046681f4a190b75b5ee
LigthAlexey <karalkin@mail.ru>
2022-08-10 22:22:23 +0700

    Обновил коммит в Git

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose user language")
