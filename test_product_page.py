aa5ad7710517014c7e8e6c39c3e6650540d1094e
LigthAlexey <karalkin@mail.ru>
2022-08-10 19:21:10 +0700

    # Короткий вариант без xfail от Vladimir Danilyuk

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,11 @@ import pytest
 from .pages.product_page import SubmitPage
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+# Короткий вариант без xfail от Vladimir Danilyuk
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+
+"""@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -12,6 +16,8 @@ from .pages.product_page import SubmitPage
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+"""
+@pytest.mark.parametrize('link', urls)
 def test_guest_can_add_product_to_basket(browser, link):
     page = SubmitPage(browser, link)
     page.open()
