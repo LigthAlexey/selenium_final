757c27eafbb169078b590980135d3106866c4501
LigthAlexey <karalkin@mail.ru>
2022-09-28 22:37:08 +0700

    Добавил BasketPage, дальше пойдёт реализация

--- test_main_page.py
+++ test_main_page.py
@@ -18,3 +18,6 @@ class TestLoginFromMainPage():
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
+
+    def test_guest_cant_see_product_in_basket_opened_from_main_page:
+
--- test_product_page.py
+++ test_product_page.py
@@ -85,3 +85,7 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page:
+
+
